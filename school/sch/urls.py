522e3afb766c76aa5d601f78b5bdc96f422e37bd
KiranGhumare <51757037+KiranGhumare@users.noreply.github.com>
2020-11-22 13:22:15 +0530

    Update urls.py

--- school/sch/urls.py
+++ school/sch/urls.py
@@ -9,4 +9,7 @@ urlpatterns= [
 	path('contact/', views.contact, name="contact"),
 	path('about/', views.about, name="about"),
 	path('login/', views.loginpage, name="login"),
+	path('category/', views.category, name="category"),
+	path('category/<str:gender_name>', views.category_gender, name="category_gender"),
+	path('school_page/<str:pk>',views.school_page,name="school_page"),
 ]
