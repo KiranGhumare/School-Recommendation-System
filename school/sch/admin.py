9a0d7f5fc90a8b282b1b74dc365da1010b1f1e5c
KiranGhumare <51757037+KiranGhumare@users.noreply.github.com>
2020-11-22 13:18:02 +0530

    Update admin.py

--- school/sch/admin.py
+++ school/sch/admin.py
@@ -2,3 +2,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 admin.site.register(Customer)
+admin.site.register(School_info)
+admin.site.register(Contact)
+admin.site.register(Board_allowed)
+admin.site.register(gender)
+admin.site.register(Facility)
