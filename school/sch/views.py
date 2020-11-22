4f2bb3649a792c680ea59b4c68ffbd4a90c609f1
KiranGhumare <51757037+KiranGhumare@users.noreply.github.com>
2020-11-22 13:23:32 +0530

    Update views.py

--- school/sch/views.py
+++ school/sch/views.py
@@ -12,7 +12,7 @@ from .decorators import unauthenticated_user,allowed_users,admin_only
 def home(request):
 	return render(request, 'sch/index.html')
 def contact(request):
-	return render(request, 'sch/contact.html')
+	return render(request, 'sch/contact.html')         
 def register(request):
 	
 	form= CreateUserForm()
@@ -59,3 +59,43 @@ def loginpage(request):
 
 def about(request):
 	return render(request, 'sch/about.html')
+def category(request):
+	return render(request, 'sch/category.html')
+def category_gender(request,gender_name):
+    gens=gender.objects.all()
+    boards=Board_allowed.objects.all()
+    items=School_info.objects.all()
+    facs=Facility.objects.all()
+    print(request.GET)
+    gend=request.GET.get('ty')
+
+    if gend=="Co-Ed School" or gend=="Only Boys School" or gend=="Only Girls School":
+    	tys=items.filter(gender_allowed=gend)
+    	return render(request, 'sch/category.html',{'gens':gens,'tys':tys,'boards':boards,'facs':facs})
+    elif gend=="CBSE" or gend=="IGCSE" or gend=="IB" or gend=="State Board" or gend=="ICSE":
+    	tys=items.filter(board=gend)
+    	return render(request, 'sch/category.html',{'gens':gens,'tys':tys,'boards':boards,'facs':facs})
+    elif gend=="AC Classrooms" or gend=="Transportation" or gend=="Swimming Pool":
+    	if(gend=="AC Classrooms"):
+    		tys=items.filter(ac_classes="Yes ")
+    		print(tys)
+    	elif gend=="Transportation":
+    		tys=items.filter(transportation="Yes ")
+    	elif gend=="Swimming Pool":
+    		tys=items.filter(swimming_pool="Yes ")
+    	return render(request, 'sch/category.html',{'gens':gens,'tys':tys,'boards':boards,'facs':facs})
+
+    return render(request, 'sch/category.html', {'items':items,'gens':gens,'boards':boards,'facs':facs})
+
+def category(request):
+    gens=gender.objects.all()
+    items = School_info.objects.all()
+    boards=Board_allowed.objects.all()
+    facs=Facility.objects.all()
+    return render(request, 'sch/category.html', {'items':items,'gens':gens,'boards':boards,'facs':facs})
+
+def school_page(request,pk):
+	item= School_info.objects.get(id=pk)
+	
+	return render(request,'sch/school_page.html',{'item':item})
+
